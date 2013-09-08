f7880aa7820a91840034599ec0a6ef87e5d166c8
Andrew Ross <andrew@vermonster.com>
2013-09-08 14:23:00 -0400

    refactors to Player

--- models/player.rb
+++ models/player.rb
@@ -8,7 +8,7 @@ class Player < Catan
     @points = 0
     @n_settlements = 0
     @n_roads = 0
-    RESOURCE_CARDS.each{|r| instance_variable_set "@#{r}", 0 }
+    RESOURCE_CARDS.each{|r| send("#{r}=", 0) }
   end
 
   def inspect
@@ -47,36 +47,33 @@ class Player < Catan
       error 'No road leading to settlement' unless board.road_to?(hex1, hex2, hex3, color)
       error 'Not enough resources to build settlement' unless [sheep, wheat, brick, wood].all?{|r| r >= 1}
     end
+    board.settlements << Settlement.new(hex1, hex2, hex3, self)
+    @n_settlements += 1
+    @points += 1
     @sheep -= 1
     @wheat -= 1
     @brick -= 1
     @wood -= 1
-    settlement = Settlement.new(hex1, hex2, hex3, self)
-    board.settlements << settlement
-    @n_settlements += 1
-    @points += 1
   end
 
   def build_road(hex1, hex2, startTurn=false)
     error 'Already built 15 roads' if n_roads >= 15
     error 'Road not buildable there' unless board.road_buildable_at?(hex1, hex2, color)
     error 'Not enough resources to build road' unless startTurn || (wood >= 1 && brick >= 1)
+    board.roads << Road.new(hex1, hex2, color)
+    board.check_for_longest_road(self)
+    @n_roads += 1
     @brick -= 1
     @wood -= 1
-    road = Road.new(hex1, hex2, color)
-    board.roads << road
-    @n_roads += 1
-    board.check_for_longest_road(self)
   end
 
   def build_city(hex1, hex2, hex3)
     error 'No settlement at location' unless board.settlement_at?(hex1, hex2, hex3, color, true)
     error 'Not enough resources to build city' unless ore >= 3 && wheat >= 2
-    @ore -= 3
-    @wheat -= 2
-    s = board.settlement_at(hex1, hex2, hex3)
-    s.size = 2
+    board.settlement_at(hex1, hex2, hex3).size = 2
     @n_settlements -= 1
     @points += 1
+    @wheat -= 2
+    @ore -= 3
   end
 end
