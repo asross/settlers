f7610d39c05c80a58e147838741bf95a37ed081a
Andrew Ross <andrew@vermonster.com>
2013-09-07 22:49:58 -0400

    make views specific to players

--- webapp.rb
+++ webapp.rb
@@ -4,17 +4,25 @@ Dir.glob('./models/*.rb').each { |f| require f }
 
 $game = Game.new
 
-get '/' do
+before do
   @game = $game
+  @color = params['color'] || old_params['color']
+  @player = @game.players.detect{|p| p.color == @color }
+  redirect "?color=#{@game.players.sample.color}" unless @player
+end
+
+get '/' do
   @board = @game.board
   @messages = @game.messages
-  @player = @game.players.sample
   erb :board
 end
 
 post '/messages' do
-  if params['message'].to_s.size > 0
-    $game.messages.unshift params['message']
-  end
-  redirect '/'
+  @game.messages << [@color, params['message']] if params['message'].to_s.size > 0
+  redirect request.referer
+end
+
+def old_params
+  return {} unless request.referer
+  Rack::Utils.parse_nested_query(URI.parse(request.referer).query)
 end
