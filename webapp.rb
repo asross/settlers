348b15efc32c272a4a44e645c6e3834323849732
Andrew Ross <andrewross@Andrew-Rosss-MacBook.local>
2013-09-15 09:10:36 -0400

    replace before block with a method to find current player

--- webapp.rb
+++ webapp.rb
@@ -5,23 +5,26 @@ Dir.glob('./models/*.rb').each { |f| require f }
 
 $game = Game.new
 
-before do
-  @game = $game
-  @color = params['color'] || old_params['color']
-  @player = @game.players.detect{|p| p.color == @color }
-  redirect "?color=#{@game.players.sample.color}" unless @player
-end
-
 get '/' do
+  @game = $game
+  @player = current_player
+  redirect "/?color=#{$game.players.sample.color}" unless @player
   erb :board
 end
 
 post '/messages' do
-  @game.messages << [@color, params['message']] if params['message'].to_s.size > 0
+  $game.messages << [current_player.color, params['message']] if params['message']
   redirect request.referer
 end
 
 def old_params
   return {} unless request.referer
   Rack::Utils.parse_nested_query(URI.parse(request.referer).query)
+end
+
+def current_player
+  color = params['color'] || old_params['color'] 
+  player = $game.players.detect{|p| p.color == color }
+  _player = $game.players.detect{|p| p.color == color }
+  player
 end
