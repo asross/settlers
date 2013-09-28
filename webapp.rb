a453d11cdf203dced9e1bac7d36646e10f72ff91
Andrew Ross <andrewross@Andrew-Rosss-MacBook.local>
2013-09-28 09:07:18 -0400

    first pass at websockets

--- webapp.rb
+++ webapp.rb
@@ -1,33 +1,56 @@
 require 'sinatra'
+require 'em-websocket'
+require 'thin'
 require 'json'
 require 'pry'
 require_relative 'models/catan'
 Dir.glob('./models/*.rb').each { |f| require f }
 
 $game = Game.new
+$channel = EM::Channel.new
 
-get '/' do
-  @game = $game
-  redirect "/?color=#{$game.players.sample.color}" unless current_player
-  erb :game
-end
+EM.run do
+  class App < Sinatra::Base
+    get '/' do
+      @game = $game
+      redirect "/?color=#{$game.players.sample.color}" unless current_player
+      erb :game
+    end
 
-post '/messages' do
-  $game.messages << [current_player.color, params['message']] if params['message']
-  redirect request.referer
-end
+    post '/messages' do
+      @game = $game
+      $game.messages << [current_player.color, params['message']]
+      $channel.push JSON.generate(['message', { html: erb(:messages) }])
+      redirect request.referer
+    end
 
-post '/actions' do
-  data = JSON.parse(params[:data])
-  begin
-    $game.perform_action(current_player, data['action'], data['args'])
-    status 200
-  rescue CatanError => e
-    status 400
-    body e.message
+    post '/actions' do
+      @game = $game
+      data = JSON.parse(params[:data])
+      begin
+        $game.perform_action(current_player, data['action'], data['args'])
+        $channel.push JSON.generate(['action', { html: erb(:board) }])
+        status 200
+      rescue CatanError => e
+        body e.message
+        status 400
+      end
+    end
+
+    def current_player
+      @current_player ||= $game.players.detect{|p| p.color == params['color']}
+    end
   end
-end
 
-def current_player
-  @current_player ||= $game.players.detect{|p| p.color == params['color']}
+  EM::WebSocket.start(host: '0.0.0.0', port: 8080) { |ws|
+    ws.onopen {
+      sid = $channel.subscribe { |msg| ws.send msg }
+
+      ws.onclose {
+        $channel.unsubscribe(sid)
+      }
+    }
+  }
+
+  Thin::Server.start(App, '0.0.0.0', 4567)
 end
