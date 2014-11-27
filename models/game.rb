7e75d618209033e010a0d7a3c1980e2fa45ce80c
Andrew Ross <andrew@vermonster.com>
2014-11-26 20:51:57 -0500

    simpler message format

--- models/game.rb
+++ models/game.rb
@@ -100,7 +100,7 @@ class Game < Catan
 
     send(action, player, *args)
 
-    @messages << "** #{player.color} performed #{action}!"
+    @messages << "#{player.color} performed #{action}!"
   end
 
   private
